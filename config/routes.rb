c9046fbd776e77aa9a16368c05a5ac9371780ae5
sanfilippomike <sanfilippomike@gmail.com>
2015-08-21 17:21:37 -0700

    updated dashboard
    updated dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -106,4 +106,5 @@ Myapp::Application.routes.draw do
 
   match "/insurance" => "appviews#insurance", via: [:get, :post]
   match "/waiver" => "appviews#waiver", via: [:get, :post]
+  match "/search" => "appviews#search", via: [:get, :post]
 end
